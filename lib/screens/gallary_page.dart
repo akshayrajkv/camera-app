1a8b7eef3e34ed0f67658ca3d4eede7cc469122b
akshayrajkv <115088147+akshayrajkv@users.noreply.github.com>
2025-04-10 17:14:42 +0530

    Update gallary_page.dart

--- lib/screens/gallary_page.dart
+++ lib/screens/gallary_page.dart
@@ -43,7 +43,7 @@ class _GallaryPageState extends State<GallaryPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Gallery Page',
+          'Gallery ',
           style: TextStyle(fontSize: 27),
         ),
         centerTitle: true,
